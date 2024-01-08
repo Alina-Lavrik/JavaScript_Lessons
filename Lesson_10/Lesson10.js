830c5b75be1d38018143d0dfba7ad76d8a0dcc4f
Alina Lavrik <alina1voro@gmail.com>
2024-01-08 22:21:03 +0300

    Add hometask

--- Lesson_10/Lesson10.js
+++ Lesson_10/Lesson10.js
@@ -118,3 +118,124 @@ const riddles = {
 riddles.askQuestion();
 
 
+// ДЗ Задание 1 Необходимо в консоль вывести все значения больше или равные 3.
+
+const numbers = { 
+    keyin1: 1, 
+    keyin2: 2, 
+    keyin3: 3, 
+    keyin4: 4, 
+    keyin5: 5, 
+    keyin6: 6, 
+    keyin7: 7, 
+}
+
+for (const key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
+
+// ДЗ Задание 2 Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
+
+const post = { 
+    author: "John", // вывести этот текст 
+    postId: 23, 
+    comments: [ 
+        { 
+        userId: 10, 
+        userName: "Alex", 
+        text: "lorem ipsum", 
+        rating: { 
+            likes: 10, 
+            dislikes: 2, // вывести это число 
+            }, 
+        }, 
+        { 
+        userId: 5, // вывести это число
+        userName: "Jane", 
+        text: "lorem ipsum 2", // вывести этот текст 
+            rating: { 
+            likes: 3, 
+            dislikes: 1, 
+            }, 
+        }, 
+    ], 
+}; 
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+// Вариант 2
+console.log(`${post.author}
+${post.comments[0].rating.dislikes}
+${post.comments[1].userId}
+${post.comments[1].text}
+`);
+
+//ДЗ Задание 3 Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
+
+const products2 = [ 
+    { 
+    id: 3, 
+    price: 200, 
+    }, 
+    { 
+    id: 4, 
+    price: 900, 
+    },
+    { 
+    id: 1, 
+    price: 1000, 
+    }, 
+]; 
+
+let priceDisc = "";
+
+products2.forEach(element => { 
+    priceDisc = priceDisc + " " + element.price * 0.85;
+});
+console.log(priceDisc); 
+
+// ДЗ Задание 4
+
+const products = [
+    {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg","2.jpg",],
+    },
+    {
+    id: 5,
+    price: 499,
+    photos: [],
+    },
+    {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg",],
+    },
+    {
+    id: 8,
+    price: 78,
+    },
+    ];
+
+
+let arrayWithPhoto = products.filter(elem => elem.photos ? elem.photos.length : undefined);
+console.log("[" + arrayWithPhoto[0].photos + "]" + "\n" + "[" + arrayWithPhoto[1].photos + "]" );
+
+
+products.sort((a, b) => a.price - b.price);
+console.log("[" + products[0].price + ", " + products[1].price + ", " + products[2].price + ", " + products[3].price + "]");
+
+// Задание 5
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+for (const key in en, ru) {
+        console.log(en[key] + ": " + ru[key]);
+}
